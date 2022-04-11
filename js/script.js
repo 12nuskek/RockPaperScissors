77ebdfb699de54eb5b60ae060e39efed4cbd49d9
Kurt_Nuske <nuske.kurt@gmail.com>
2022-04-11 20:59:44 +1000

    UI created need to add points board

--- js/script.js
+++ js/script.js
@@ -41,20 +41,26 @@ function playRound(playerSelection) {
         
     }
 
-
-    btn.addEventListener('click', function (e) {
-        console.log(e);
-      });
-      
-      btn.addEventListener('click', function (e) {
-        e.target.style.background = 'blue';
-      });
 function alertFunction() {
-  alert("YAY! YOU DID IT!");
-}
+        alert("YAY! YOU DID IT!");
+      }
+      
+      
+scissors.addEventListener('click', function (e) {
+        e.target.style.background = 'blue';
+        console.log(e);
+        alertFunction()
+      });
 
-// METHOD 2
-btn.onclick = alertFunction;
 
-// METHOD 3
-btn.addEventListener('click', alertFunction);
+paper.addEventListener('click', function (e) {
+        e.target.style.background = 'blue';
+        console.log(e);
+        alertFunction()
+      });
+
+rock.addEventListener('click', function (e) {
+        e.target.style.background = 'blue';
+        console.log(e);
+        alertFunction()
+      });
