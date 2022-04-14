f935e6f8cfc68a1dec111ce5c45e6497d24f4122
Kurt Nuske <kurt.nuske@cpl.org.au>
2022-04-15 09:07:09 +1000

    made js logic changes

--- js/script.js
+++ js/script.js
@@ -2,8 +2,31 @@ var userscore = 0;
 var computerscore = 0;
 
 
+scissors.addEventListener('click', function (e) {
+    playerSelection = "Scissors";
+    alertFunction();
+    choices();
+  });
+
+
+paper.addEventListener('click', function (e) {
+    playerSelection = "Paper";
+    alertFunction();
+    choices()
+  });
+
+rock.addEventListener('click', function (e) {
+    playerSelection = "Rock";
+    alertFunction();
+    choices()
+  });
+
+
 function alertFunction() {
 
+    while (count <= 5) {
+    
+    count++;
     const spr = ["Scissors", "Paper", "Rock"];
 
     const random = Math.floor(Math.random() * spr.length);
@@ -11,29 +34,28 @@ function alertFunction() {
     
 
     if (playerSelection == "Scissors" && computer == "Paper") {
-        alert("Scissors beats paper user wins!");
         userscore++;
         console.log(userscore);
-
         const userscoreboard = document.querySelector('#userscore');
         console.log(userscoreboard);
         userscoreboard.textContent = userscore;
         }
+
     if (playerSelection == "Scissors" && computer == "Rock") {
-        alert("Rock beats Scissors Computer wins!");
         computerscore++;
         console.log(computerscore);
-        const userscoreboard = document.querySelector('#computerscore');
-        console.log(userscoreboard);
-        userscoreboard.textContent = userscore;
-
+        const computerscoreboard = document.querySelector('#computerscore');
+        console.log(computerscoreboard);
+        computerscoreboard.textContent = computerscore;
         }
     if (playerSelection == "Scissors" && computer == "Scissors") {
         alert("We've got a draw on our hands");
         }        
 
+
+
+
     if (playerSelection == "Paper" && computer == "Rock") {
-        alert("Paper beats Rock User wins!");
         userscore++;
         console.log(userscore);
         const userscoreboard = document.querySelector('#userscore');
@@ -41,19 +63,20 @@ function alertFunction() {
         userscoreboard.textContent = userscore;
         }
     if (playerSelection == "Paper" && computer == "Scissors") {
-            console.log("Scissors beats paper Computer wins!");
             computerscore++;
             console.log(computerscore);
-            const userscoreboard = document.querySelector('#computerscore');
-            console.log(userscoreboard);
-            userscoreboard.textContent = userscore;
+            const computerscoreboard = document.querySelector('#computerscore');
+            console.log(computerscoreboard);
+            computerscoreboard.textContent = computerscore;;
         }        
     if (playerSelection == "Paper" && computer == "Paper") {
         alert("We've got a draw on our hands");
         }   
         
+
+
+
     if (playerSelection == "Rock" && computer == "Scissors") {
-        alert("Rock beats Scissors User wins!");
         userscore++;
         console.log(userscore);
         const userscoreboard = document.querySelector('#userscore');
@@ -61,18 +84,21 @@ function alertFunction() {
         userscoreboard.textContent = userscore;
         }
     if (playerSelection == "Rock" && computer == "Paper") {
-        alert("paper beats paper rock computer wins!");
         computerscore++;
         console.log(computerscore);
-        const userscoreboard = document.querySelector('#computerscore');
-        console.log(userscoreboard);
-        userscoreboard.textContent = userscore;
+        const computerscoreboard = document.querySelector('#computerscore');
+        console.log(computerscoreboard);
+        computerscoreboard.textContent = computerscore;;
         }        
     if (playerSelection == "Rock" && computer == "Rock") {
         alert("We've got a draw on our hands");
         }     
     }
-      
+    if(count = 5){
+        console.log("Gameover")
+
+    }
+ }
 
 function choices() {
 
@@ -122,24 +148,3 @@ function choices() {
 
     }
       
-scissors.addEventListener('click', function (e) {
-        console.log(e);
-        playerSelection = "Scissors";
-        alertFunction();
-        choices();
-      });
-
-
-paper.addEventListener('click', function (e) {
-        console.log(e);
-        playerSelection = "Paper";
-        alertFunction();
-        choices()
-      });
-
-rock.addEventListener('click', function (e) {
-        console.log(e);
-        playerSelection = "Rock";
-        alertFunction();
-        choices()
-      });
